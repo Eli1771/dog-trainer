252b4d4627b96a5bf09cd3e1928111e74eb1c20d
github username <github email address>
2020-01-13 12:40:37 -0800

    User confirms before deleting dogs

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -3,13 +3,18 @@ import React from 'react';
 const Dog = props => {
   const { name, breed, dob, id } = props.dog;
 
+  const handleDelete = () => {
+    if (window.confirm(`Remove ${name} from your records?`)) {
+      props.removeDog(id)
+    }
+  }
 
   return(
     <div className="component">
       <h1>{name}</h1>
       <p>{dob} - {breed}</p>
       <button
-        onClick={() => props.removeDog(id)}
+        onClick={handleDelete}
       >Delete</button>
     </div>
   );
