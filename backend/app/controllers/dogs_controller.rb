11821b86b2017d5b7555fa98520e3200b422b43e
github username <github email address>
2020-01-14 12:24:09 -0800

    First controller actions for dog API

--- backend/app/controllers/dogs_controller.rb
+++ backend/app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+  def index
+    dogs = Dog.all
+    render json: dogs
+  end
+
+  def show
+    dog = Dog.find_by(id: params[:id])
+    render json: dog
+  end
 end
