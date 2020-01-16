e631b3391a8fa92e30a8b9052234cf61fd973247
github username <github email address>
2020-01-16 10:16:38 -0800

    Stubbed DogShow sub-container

---
+++ frontend/src/containers/dogs/DogShow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Dog from '../../components/dogs/Dog';
+import { DogsSidebar } from './DogsSidebar'
+
+export default class DogShow extends Component {
+  render() {
+    return(
+      <div>
+      </div>
+    );
+  }
+}
