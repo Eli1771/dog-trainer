acfc32e58fef0632f24eb6d72b8f092e50861c7b
github username <github email address>
2020-01-16 10:17:50 -0800

    Filled out DogShow

--- frontend/src/containers/dogs/DogShow.js
+++ frontend/src/containers/dogs/DogShow.js
@@ -5,8 +5,13 @@ import { DogsSidebar } from './DogsSidebar'
 
 export default class DogShow extends Component {
   render() {
+    const { dogs, match } = this.props;
+    const dog = dogs.find(dog => dog.id === parseInt(match.params.dogId));
+    console.log('dog: ', dog);
     return(
       <div>
+        <Dog dog={dog} />
+        <DogsSidebar dogs={dogs} currentDogId={dog.id} />
       </div>
     );
   }
