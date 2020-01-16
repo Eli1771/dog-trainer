b642dbac4ee1a63a06317c0c1e514ec1849ab313
github username <github email address>
2020-01-16 10:17:35 -0800

    Component to display dogs not being viewed in sidebare for detail view

---
+++ frontend/src/containers/dogs/DogsSidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Dog from '../../components/dogs/Dog';
+
+export const DogsSidebar = props => {
+  const { dogs, currentDogId } = props;
+  const filteredDogs = dogs.filter(dog => dog.id !== currentDogId);
+  return(
+    <div>
+      {filteredDogs.map(dog => <Dog key={dog.id} dog={dog} />)}
+    </div>
+  );
+}
