517f3b043d4fdcd304451043af523e06302bd4e4
github username <github email address>
2020-01-20 14:07:10 -0500

    Set up skills component for dynamic rendering

--- frontend/src/containers/skills/Skills.js
+++ frontend/src/containers/skills/Skills.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import Skill from './Skill';
+import { fetchSkills } from '../../actions/dogs';
 
 class Skills extends Component {
+  componentDidMount() {
+    this.props.fetchSkills();
+  }
+
+  renderSkills = skills => {
+    return skills.map(skill => {
+      return <Skill key={skill.id} skill={skill} />
+    });
+  }
+
   render() {
     const { skills, dogId } = this.props
-    const filteredSkills = skills.filter(s => s.dogId = dogId);
-    console.log('all skills: ', skills);
-    console.log(filteredSkills);
+    const filteredSkills = skills.filter(s => s.dog_id === dogId);
     return(
       <div>
         <h3>Skills</h3>
-        {filteredSkills.map(skill => <Skill key={skill.id} skill={skill} />)}
+        {this.renderSkills(filteredSkills)}
       </div>
     );
   }
@@ -23,4 +33,9 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Skills);
+export default connect(mapStateToProps, { fetchSkills })(Skills);
+
+
+// return dogs.map(dog => {
+//   return <Dog key={dog.id} dog={dog} removeDog={this.props.removeDog} />
+// });
