7383f36e699f5ccbc0ff5a311d2f4a461a3bf3d4
github username <github email address>
2020-01-22 19:22:11 -0500

    DogHeader stateless comp for easier styling

---
+++ frontend/src/components/dogs/DogHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DogHeader = props => {
+  const { name, breed, dob } = props.dog;
+
+  return(
+    <div className="component dog-header">
+      <h1>{name}</h1>
+      <p>{dob} - {breed}</p>
+    </div>
+  );
+}
+
+export default DogHeader;
