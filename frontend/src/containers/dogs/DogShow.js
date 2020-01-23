670a160779c79ff5865d6766094a2df51b0d1f04
github username <github email address>
2020-01-22 19:23:08 -0500

    Utilize DogHeader comp in DogShow

--- frontend/src/containers/dogs/DogShow.js
+++ frontend/src/containers/dogs/DogShow.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchDogs } from '../../actions/dogs';
-import Dog from '../../components/dogs/Dog';
+import DogHeader from '../../components/dogs/DogHeader';
 import { DogsSidebar } from './DogsSidebar';
 import Skills from '../skills/Skills';
 
 class DogShow extends Component {
 
-  componentDidMount() {
-    this.props.fetchDogs()
-  }
+  // componentDidMount() {
+  //   this.props.fetchDogs()
+  // }
 
   render() {
     const { dogs, match } = this.props;
     const dog = dogs.find(dog => dog.id.toString() === match.params.dogId.toString());
     console.log('dog: ', dog);
     return(
-      <div>
-        <Dog dog={dog} />
+      <div className="dog-show">
+        <DogHeader dog={dog} />
         <Skills dogId={dog.id} />
         <DogsSidebar dogs={dogs} currentDogId={dog.id} />
       </div>
