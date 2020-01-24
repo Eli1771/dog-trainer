8db941a7df6862011afb9210e3624b8fe4965d10
github username <github email address>
2020-01-24 13:06:00 -0800

    Starting change R.R. in skill w/ CSS Class help

--- frontend/src/containers/skills/Skill.js
+++ frontend/src/containers/skills/Skill.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Skill extends Component {
+
+
+  editRate = e => {
+
+  }
+
   render() {
     const skill = this.props.skill
     return(
       <div className="skill">
         <h2>{skill.name}</h2>
-        <p>{skill.description} - {skill.reward_rate}</p>
+        <p>{skill.description}</p>
+        <p>{skill.reward_rate}</p>
+        <button
+          onClick={this.editRate}
+        >Change</button>
+        <select className="hidden">
+          <option value="1">Every Time</option>
+          <option value="2">Every Other Time</option>
+        </select>
+        <button
+          onclick={this.updateRate}
+          className="hidden"
+        >Save</button>
       </div>
     )
   }
