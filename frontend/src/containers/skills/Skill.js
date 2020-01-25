7d8d2f741fcc0ac580be1276462212a7579331ad
github username <github email address>
2020-01-24 21:35:10 -0800

    Stubbed update handler and tweaked select box

--- frontend/src/containers/skills/Skill.js
+++ frontend/src/containers/skills/Skill.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 
 export default class Skill extends Component {
 
-  rewardRates = ['Every Time', 'Every Other Time', 'Every third Time', 'Every Several Times', 'Seldom/Randomly'];
+  rewardRates = ['-','Every Time', 'Every Other Time', 'Every third Time', 'Every Several Times', 'Seldom/Randomly'];
+
+  skillId = this.props.skill.id;
 
   renderRewardOptions = () => {
     return this.rewardRates.map((rate, idx) => {
-      return <option value={idx + 1}>{rate}</option>
+      return <option key={idx} value={idx}>{rate}</option>
     });
   }
 
+  editRate = e => {
+    e.target.classList.add('hidden');
+    document.querySelector(`._${this.skillId}.select-rate`).classList.remove('hidden');
+    document.querySelector(`._${this.skillId}.update-rate`).classList.remove('hidden');
+  }
+
+  updateRate = e => {
+    e.target.classList.add('hidden');
+    document.querySelector(`._${this.skillId}.select-rate`).classList.add('hidden');
+    document.querySelector(`._${this.skillId}.edit-rate`).classList.remove('hidden');
+
+    if (!e.target.value === 0) {
+      console.log('attempting to update state!');
+      //send callback prop to update state
+    }
   }
 
   render() {
@@ -18,7 +35,7 @@ export default class Skill extends Component {
       <div className="skill">
         <h2>{skill.name}</h2>
         <p>{skill.description}</p>
-        <p>{this.rewardRates[skill.reward_rate - 1]}</p>
+        <p>{this.rewardRates[skill.reward_rate]}</p>
         <button
           className={`_${skill.id} edit-rate`}
           onClick={this.editRate}
@@ -27,7 +44,7 @@ export default class Skill extends Component {
           {this.renderRewardOptions()}
         </select>
         <button
-          onclick={this.updateRate}
+          onClick={this.updateRate}
           className={`_${skill.id} update-rate hidden`}
         >Save</button>
       </div>
