b9b977407ec5bdd8969a245404d05e7c9a901dd0
github username <github email address>
2020-01-25 12:23:17 -0800

    Add and remove handlers in Skills reducer

--- frontend/src/reducers/skillsReducer.js
+++ frontend/src/reducers/skillsReducer.js
@@ -4,6 +4,23 @@ const skillsReducer = (state = {
   skills: []
 }, action) => {
   switch (action.type) {
+
+    case 'ADD_SKILL':
+      const skill = {
+        ...action.skill,
+        id: uuid()
+      }
+      return {
+        ...state,
+        skills: [ ...state.skills, skill ]
+      }
+
+    case 'REMOVE_SKILL':
+      return {
+        ...state,
+        skills: state.skills.filter(skill => skill.id !== action.skillId)
+      }
+
     case 'ADD_SKILLS':
       console.log(action.skills);
       return { ...state, skills: action.skills, loading: false }
