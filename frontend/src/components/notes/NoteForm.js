f917bd6dd783de8cdd6a244786c36421db3aaf4e
github username <github email address>
2020-01-26 06:46:14 -0800

    Set up event handlers, working backwards

--- frontend/src/components/notes/NoteForm.js
+++ frontend/src/components/notes/NoteForm.js
@@ -10,12 +10,25 @@ export default class NoteForm extends Component {
   }
 
   handleChange = e => {
-    
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    });
+    e.target.value = this.state[e.target.name];
   }
 
   handleSubmit = e => {
     e.preventDefault();
-
+    const content = this.state.content;
+    const timeStamp = moment().format('ddd, MMM Do - h:mm a');
+    console.log('timestamp: ', timeStamp)
+    // this.props.addNote({
+    //   content: content,
+    //   time_stamp: timeStamp
+    // });
+    this.setState({
+      content: ''
+    });
   }
 
   render() {
