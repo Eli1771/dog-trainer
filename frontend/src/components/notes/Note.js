666e8dba03fac57b751c75afc9bf06ad16bda3e8
github username <github email address>
2020-01-26 20:59:39 -0800

    Addnote function successfully adding note to store, need to fix render

--- frontend/src/components/notes/Note.js
+++ frontend/src/components/notes/Note.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Note = props => {
   return(
     <div className="note">
-      <p>props.timestamp</p>
-      <p>props.content</p>
+      <p>{props.timestamp}</p>
+      <p>{props.content}</p>
     </div>
   )
 }
+
+export default Note;
--- frontend/src/containers/notes/Notes.js
+++ frontend/src/containers/notes/Notes.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import NoteForm from '../../components/notes/NoteForm';
+import Note from '../../components/notes/Note';
 import { addNote } from '../../actions/notes';
 
 class Notes extends Component {
-  // renderNotes = notes => {
-  //   return notes.map()
-  // }
+  renderNotes = notes => {
+    return notes.map(note => {
+      return <Note key={note.id} note={note} />
+    })
+  }
 
   render() {
     console.log('all notes: ', this.props.notes);
     return(
       <div className="notes-container">
         <h3>Progress Log</h3>
-        <NoteForm addNote={addNote} />
+        {this.renderNotes(this.props.notes)}
+        <NoteForm addNote={this.props.addNote} />
       </div>
     );
   }
