c2e56481efcb24ee1de6bfa40e3d67b5b02206cf
github username <github email address>
2020-01-28 11:01:58 -0800

    Notes controller basic handlers and strong params

--- backend/app/controllers/notes_controller.rb
+++ backend/app/controllers/notes_controller.rb
@@ -1,2 +1,17 @@
 class NotesController < ApplicationController
+  def create
+    note = Note.create(note_params)
+    render json: note
+  end
+
+  def index
+    notes = Note.all
+    render json: notes
+  end 
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content, :timestamp, :dog_id)
+  end
 end
