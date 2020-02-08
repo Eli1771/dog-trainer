66e70a8fe8194af7a456ba30762215d3f1ecb4a5
github username <github email address>
2020-02-07 23:37:50 -0800

    Refactored home comp as actuall React component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export const Home = props => {
-  return(
-    <div className="welcome">
-      <h1>Workin Dog</h1>
-      <Link to='/dogs'>View Your Dogs</Link>
-      <Link to='/log'>View Your Progress</Link>
-    </div>
-  );
+export default class Home extends Component {
+  render() {
+    return(
+      <div className="welcome">
+        <h1>Workin Dog</h1>
+        <Link to='/dogs'>View Your Dogs</Link>
+        <Link to='/log'>View Your Progress</Link>
+      </div>
+    );
+  }
 }
