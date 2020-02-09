70879c69476df70926dbab812d4350ff52d03e50
github username <github email address>
2020-02-09 09:57:06 -0800

    Wrote crazy loop to populate page w/ bones

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -14,33 +14,32 @@ export const Home = props => {
   }
 
   const renderBackground = () => {
-    console.log(window.innerWidth)
     //calculate how many bones will fill the screen across, add one
+    let across = Math.ceil((window.innerWidth / 200) + 3);
     //calculate height of h1 field by vh
+    let height = Math.ceil((window.innerHeight*0.39));
+    let down = Math.ceil((height / 200) + 3);
     //calculate how many rows of bones will be needed, add one
+    const upOrDown = ['up', 'down'];
+    let r = []
+    for (let i = 0; i < down; i++) {
+      let offset = 0
+      //if even number in each row, offset is always 0
+      if (across % 2 === 1) {
+        offset = i;
+      }
 
-    return(
-      <>
+      for (let j = 0; j < across; j++) {
+        let k = j + offset;
+        let dir = k > 1 ? upOrDown[k % 2] : upOrDown[k];
+        r.push(<img src={boneOutline} alt="bone-outline" className={`move-${dir}`}/>);
+      }
+    }
 
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-        <img src={boneOutline} alt="bone-outline" className="move-up"/>
-        <img src={boneOutline} alt="bone-outline" className="move-down"/>
-      </>
-    )
+    return r;
   }
+
+
 
   return(
     <div className="welcome">
